9a5cc7cb337389436aea56bb39e0cbe6d0c7ddc8
ammilam <andrewmichaelmilam@gmail.com>
2020-11-26 21:29:41 -0600

    Delete test.sh

--- test.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-export USERNAME=ammilam
-export EMAIL=andrewmichaelmilam@gmail.com
-export REPO=mac-intro
-sed "s/USERNAME/$USERNAME/g; s/EMAIL/$EMAIL/g; s/REPO/$REPO/g" ./flux/flux.yaml > test.yaml
