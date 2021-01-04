fa55c6f1d2e127a1303c0d44d2341d2788576c09
Andrew Milam <Andrew.Milam@davita.com>
2021-01-04 13:44:11 -0600

    fixing cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -11,11 +11,11 @@ NAME=$(cat terraform.tfstate|jq -r '.outputs.cluster_name.value')
 PROJECT=$(cat terraform.tfstate|jq -r '.outputs.project_id.value')
 export GOOGLE_APPLICATION_CREDENTIALS=./$SA_NAME
 terraform destroy -var "google_credentials=${SA_NAME}" -var "repo=${REPO}" -var "github_token=${TOKEN}" -var "username=${USERNAME}" -var "email_address=${EMAIL}" -var "cluster_name=${NAME}" -var "project_id=${PROJECT}" -auto-approve
-gcloud compute disks list --format=json|grep $NAME|
+gcloud compute disks list --format=json|
 jq --raw-output '.[] | "\(.name)|\(.zone)"' $PDB_FILE|
 while IFS="|" read -r name zone; do
 echo name=$name zone=$(echo $zone|awk '{print $NF}' FS=/)
-gcloud compute disks delete $name --zone=$zone
+gcloud compute disks delete $(echo $name|grep $NAME) --zone=$(echo $zone|awk '{print $NF}' FS=/)
 done
 
 read -p 'Do you want to delete the Service Account json? ' p
